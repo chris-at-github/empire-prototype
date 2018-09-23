ac2c5d95e2bcd25f5486a14e0d67edc83fafb3f8
Chris <pschorr.christian@gmail.com>
2018-09-23 23:21:02 +0200

    0.0.7@2018-09-23

--- build/js/resources/collection.js
+++ build/js/resources/collection.js
@@ -1,121 +1,52 @@
 'use strict';
 
-import ResourceValue from './value';
-import ResourceCollectionException from 'exceptions/resourcecollection';
-
-let ResourceCollection = function() {
+let Collection = function() {
 
 	/**
 	 * @var {object}
 	 */
-	this.resources = {};
-
-	/**
-	 * @var {float}
-	 */
-	this.maxValue = 0.0;
-
-	/**
-	 * @var {int}
-	 */
-	this.maxResources = null;
+	this.items = {};
 };
 
 /**
- * Setzt das maximale Limit von Resourcen Einheiten
- * 
- * @param {float} max
- */
-ResourceCollection.prototype.setMaxValue = function(max) {
-	this.maxValue = max;
-};
-
-/**
- * prueft ob eine Aktion, die minimale oder maximale Menge ueberschreiten wuerde
- * 
- * @throws {ResourceCollectionException}
- * @param {float} value
- * @return {boolean}
- */
-ResourceCollection.prototype.checkResourceValue = function(value) {
-	if(value > this.maxValue) {
-		throw new ResourceCollectionException(ResourceCollectionException.MAX_VALUE);
-	}
-
-	if(value < 0) {
-		throw new ResourceCollectionException(ResourceCollectionException.MIN_VALUE);
-	}
-
-	return true;
-};
-
-/**
- * Fuegt ein ResourceValue Objekt der Collection hinzu
+ * Fuegt einen neuen Eintrag hinzu
  *
- * @param {object} ResourceValue
+ * @param {mixed} key
+ * @param {mixed} value
+ * @return {object} Collection
  */
-ResourceCollection.prototype.setResourceValue = function(resource) {
-	if(this.checkResourceValue(resource.value) === true) {
-		this.resources[resource.qcn] = resource;
-	}
+Collection.prototype.set = function(key, value) {
+	this.items[key] = value;
+	return this;
 };
 
 /**
- * Liefert ein ResourceValue Objekt anhand des QCN zurueck
+ * Liefert einen Eintrag anhand eines Schluessels
  *
- * @return {object} ResourceValue
+ * @return {mixed}
  */
-ResourceCollection.prototype.getResourceValue = function(qcn) {
-	if(_.isUndefined(this.resources[qcn]) === false) {
-		return this.resources[qcn];
+Collection.prototype.get = function(key) {
+	if(_.isUndefined(this.items[key]) === false) {
+		return this.items[key];
 	}
 
 	return null;
 };
 
 /**
- * Addiert ein ResourceValue Objekt zu einem bestehenden Objekt
- *
- * @param {object} ResourceValue
- */
-ResourceCollection.prototype.addResourceValue = function(resource) {
-	if(_.isUndefined(this.resources[resource.qcn]) === false) {
-		if(this.checkResourceValue(this.resources[resource.qcn].value + resource.value)) {
-			this.resources[resource.qcn].addValue(resource);
-		}
-
-	} else {
-		this.setResourceValue(resource);
-	}
-};
-
-/**
- * Subtrahiert ein ResourceValue Objekt von einem bestehenden Objekt
- *
- * @param {object} ResourceValue
- */
-ResourceCollection.prototype.subResourceValue = function(resource) {
-	if(_.isUndefined(this.resources[resource.qcn]) === false) {
-		if(this.checkResourceValue(this.resources[resource.qcn].value - resource.value)) {
-			this.resources[resource.qcn].subValue(resource);
-		}
-
-	} else {
-		this.setResourceValue(resource);
-	}
-};
-
-/**
  * Fuellt die Collection anhand eines JSON Objekts
- * 
+ *
  * @param {object} json
+ * @return {object} Collection
  */
-ResourceCollection.prototype.fill = function(json) {
+Collection.prototype.fill = function(json) {
 	let collection = this;
 
-	_.forEach(json, function(value, qcn) {
-		collection.setResourceValue(new ResourceValue(qcn, value));
+	_.forEach(json, function(value, key) {
+		collection.set(key, value);
 	});
+
+	return this;
 };
 
 /**
@@ -123,14 +54,14 @@ ResourceCollection.prototype.fill = function(json) {
  *
  * @return {object}
  */
-ResourceCollection.prototype.toJson = function() {
+Collection.prototype.toJson = function() {
 	let json = {};
 
-	_.forEach(this.resources, function(resource, qcn) {
-		json[qcn] = resource.value;
+	_.forEach(this.items, function(value, key) {
+		json[key] = value;
 	});
 
 	return json;
 };
 
-export default ResourceCollection;
+export default Collection;
