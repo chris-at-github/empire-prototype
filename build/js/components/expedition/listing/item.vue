29baf614415e9bfd2b87889c91cb33a5dd0184ab
Chris <pschorr.christian@gmail.com>
2018-10-03 16:27:51 +0200

    0.0.8@2018-10-03

--- build/js/components/expedition/listing/item.vue
+++ build/js/components/expedition/listing/item.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="card card-object">
-		<div class="card--body">
-			EXPEDITION {{properties.id}}
+		<div class="card--property">
+			<div class="properties">
+				<div class="property">
+					<div class="property--title">Typ:</div>
+					<div class="property--value">{{type}}</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -20,6 +25,14 @@
 				expedition.fill(this.properties);
 
 				return expedition;
+			},
+
+			type: function() {
+				let types = {}
+						types[Empire.expedition.TYPE_SEARCH] = 'Suche';
+						types[Empire.expedition.TYPE_TARGET] = 'Vorgegebene Resource';
+
+				return types[this.properties.type];
 			}
 		},
 
