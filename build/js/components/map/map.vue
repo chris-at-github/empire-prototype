9917ad83d9b8037c157ff8073fd6af5f7cc30257
Chris <pschorr.christian@gmail.com>
2018-10-16 23:42:04 +0200

    0.0.9@2018-10-16

--- build/js/components/map/map.vue
+++ build/js/components/map/map.vue
@@ -1,24 +1,26 @@
 <template>
-	<div class="map" v-bind:style="styleObject">
-		<!--<div class="map&#45;&#45;tile-container">-->
+	<div class="map" ref="map">
+		<div class="map--scroll-container" v-bind:style="styleObject" ref="scroll">
+			<!--<div class="map&#45;&#45;tile-container">-->
 			<!--<cs-tile-->
-				 <!--v-for="(tile, index) in tiles"-->
-				 <!--v-bind:x="tile.x"-->
-				 <!--v-bind:y="tile.y"-->
-				 <!--v-bind:terrain="tile.terrain"-->
-				 <!--v-bind:key="index">-->
+			<!--v-for="(tile, index) in tiles"-->
+			<!--v-bind:x="tile.x"-->
+			<!--v-bind:y="tile.y"-->
+			<!--v-bind:terrain="tile.terrain"-->
+			<!--v-bind:key="index">-->
 			<!--</cs-tile>-->
-		<!--</div>-->
+			<!--</div>-->
 
-		<!--<div class="map&#45;&#45;object-container">-->
+			<!--<div class="map&#45;&#45;object-container">-->
 			<!--<cs-object-->
-				<!--v-for="(object, index) in objects"-->
-				<!--v-bind:id="object.id"-->
-				<!--v-bind:position="object.position"-->
-				<!--v-bind:size="object.size"-->
-				<!--v-bind:key="index">-->
+			<!--v-for="(object, index) in objects"-->
+			<!--v-bind:id="object.id"-->
+			<!--v-bind:position="object.position"-->
+			<!--v-bind:size="object.size"-->
+			<!--v-bind:key="index">-->
 			<!--</cs-object>-->
-		<!--</div>-->
+			<!--</div>-->
+		</div>
 	</div>
 </template>
 
@@ -54,14 +56,17 @@
 
 		mounted: function() {
 			window.addEventListener('resize', this.onWindowResize);
+			this.onWindowResize();
 		},
 
 		methods: {
 
 			// @see: https://github.com/vuejs/vue/issues/1915
-			onWindowResize(event) {
-				this.windowWidth = event.currentTarget.innerWidth;
-				this.windowHeight = event.currentTarget.innerHeight;
+			onWindowResize() {
+				console.log(this.$refs.map.clientWidth);
+
+				this.windowWidth = this.$refs.map.clientWidth;
+				this.windowHeight = this.$refs.map.clientHeight;
 			},
 
 			getMapCenterPosition: function() {
@@ -101,26 +106,19 @@
 				let object = Empire.factory.map.create(this.properties.qcn);
 						object.fill(this.properties);
 
-				// console.log(object);
-
 				return object;
 			},
 
 			styleObject: function() {
-				this.object;
+				// this.object;
+				this.getMapCenterPosition();
 
 				return {
 					'width': this.getMapWidth() + 'px',
 					'height': this.getMapHeight() + 'px',
-					'top': '0px',
-					'left': '0px'
+					'top': this.getMapCenterPosition().y + 'px',
+					'left': this.getMapCenterPosition().x + 'px'
 				};
-				// return {
-				// 	'width': this.getMapWidth() + 'px',
-				// 	'height': this.getMapHeight() + 'px',
-				// 	'top': this.getMapCenterPosition().y + 'px',
-				// 	'left': this.getMapCenterPosition().x + 'px'
-				// }
 			},
 
 			tiles: function() {
